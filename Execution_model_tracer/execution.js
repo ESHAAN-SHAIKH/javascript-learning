0499a47f76a8a33a0b87a336d3777c575798b798
ESHAAN-SHAIKH <eshaanshaikh123@gmail.com>
2026-03-19 19:31:06 +0530

    done with referecence

--- Execution_model_tracer/execution.js
+++ Execution_model_tracer/execution.js
@@ -22,3 +22,23 @@ two(callStackTrace)
 
 console.log("\n--- Part 1: Call Stack Demonstration ---");
 one([]);
+
+function demonstrateReferences(){
+    console.log("\n--- Part 2: Reference vs Value ---");
+  let a = 10;
+  let b = a;
+   b = 20;
+
+  console.log("Primitive values:");
+  console.log("a =", a, "| b =", b);
+  console.log("Changing b did NOT affect a because primitives are copied by value.\n");
+
+  let obj1 = { value: 10 };
+  let obj2 = obj1;
+  obj2.value = 20;
+
+  console.log("Object values:");
+  console.log("obj1 =", obj1, "| obj2 =", obj2);
+  console.log("Changing obj2 DID affect obj1 because objects are shared by reference.");
+}
+demonstrateReferences();
